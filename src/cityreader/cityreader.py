905203574a7e96a8f9f1cb953ecebff21e872645
Justin A <jarata@gmail.com>
2019-11-01 10:33:03 -0700

    cleaned up linter

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -21,12 +21,12 @@ class City:
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
+
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
     with open('cities.csv', 'r') as i:
         r = csv.reader(i)
         next(i)
